f98b3824ef4ddbff1d8cb058c6c880ffa0fb0661
Sam Harris <sam@jam-bc.com>
2019-11-04 12:29:24 +0000

    each project has its own basic page

--- src/views/Project.js
+++ src/views/Project.js
@@ -1,14 +1,22 @@
 import React, { Fragment } from "react";
 
-import Content from "../components/Content";
+import contentData from "../utils/contentData";
 
 const Project = props => {
-  console.log(props);
-  console.log(props.match.params.id);
+  const id = props.match.params.id;
+
+  const [projectData] = contentData.filter(project => project.id == id);
+
   return (
-    <Fragment>
-      <div>Project deets</div>
-    </Fragment>
+    <section className="project">
+      <h1 className="mb-4">{projectData.title}</h1>
+      <img
+        className="project__image"
+        src={projectData.image}
+        alt={projectData.title + " logo"}
+      />
+      <p className="lead">{projectData.description}</p>
+    </section>
   );
 };
 
