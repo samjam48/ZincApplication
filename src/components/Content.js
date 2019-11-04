f1a44ca0bd7bb20a2ab47fd1adac737bb5e95203
Sam Harris <sam@jam-bc.com>
2019-11-04 11:47:10 +0000

    fix portfolio display arrangement

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,9 +8,9 @@ import contentData from "../utils/contentData";
 class Content extends Component {
   render() {
     return (
-      <div className="next-steps my-5">
-        <h2 className="my-5 text-center">What can I do next?</h2>
-        <Row className="d-flex justify-content-between">
+      <div className="portfolio-page my-5">
+        <h2 className="my-5 text-center">Active ideas</h2>
+        <Row className="d-flex justify-content-between portfolio-projects">
           {contentData.map((col, i) => (
             <Col key={i} md={5} className="mb-4 project-card">
               <h6 className="mb-3">
