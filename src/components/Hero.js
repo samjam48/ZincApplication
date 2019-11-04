a37618d5d9723b82e81001436a5e3304d9655239
Sam Harris <sam@jam-bc.com>
2019-11-04 10:53:32 +0000

    change basic text on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,16 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <h1 className="mb-4">Demo Portfolio Manager</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      This is a sample application built to provide a basic discussion forum and
+      portfolio manager for <a href="https://www.zinc.vc/">Zinc</a> Founder
+      ideas, using <a href="https://reactjs.org">React.js</a>
+    </p>
+    <p className="lead">
+      Users can create a login, search through Zinc portfolio ideas or add their
+      own idea.
     </p>
   </div>
 );
